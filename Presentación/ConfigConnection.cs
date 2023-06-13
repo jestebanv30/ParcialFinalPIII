09425b8e67c4929be37dc09623482e5adc60be2c
jestebanv30 <valdezjuanes014@gmail.com>
2023-06-12 19:44:47 -0500

    Agregar archivos de proyecto.

---
+++ Presentación/ConfigConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentación
+{
+    public class ConfigConnection
+    {
+        public static string conexionString = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+    }
+}
